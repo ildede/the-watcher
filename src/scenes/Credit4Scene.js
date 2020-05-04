b9d03d994c7ea029b06102fe0d715a4f3685a3f8
Davide Pedron <davide@happydev.cloud>
2020-05-04 02:10:20 +0200

    Fix image of credit 4

--- src/scenes/Credit4Scene.js
+++ src/scenes/Credit4Scene.js
@@ -1,7 +1,6 @@
 import 'phaser'
 import {CREDIT_4, CREDIT_5} from "../TheWatcher";
-import creditTopPng from "../assets/title/4/4-top.png";
-import creditBottomPng from "../assets/title/4/4-bottom.png";
+import creditTopBottomPng from "../assets/title/4/4-top-bottom.png";
 import imagePng from "../assets/title/4/4-image.png";
 
 export default class Credit4Scene extends Phaser.Scene {
@@ -10,19 +9,16 @@ export default class Credit4Scene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('top4', creditTopPng)
-        this.load.image('bottom4', creditBottomPng)
+        this.load.image('topBottom4', creditTopBottomPng)
         this.load.image('image4', imagePng)
     }
 
     create() {
         this.add.image(450, 300, 'creditsBg')
-        this.add.image(250, 300, 'image4')
+        this.add.image(240, 300, 'image4')
             .setScale(0.55,0.55)
 
-        this.add.image(690, 170, 'top4')
-            .setScale(0.8, 0.8)
-        this.add.image(690, 390, 'bottom4')
+        this.add.image(685, 300, 'topBottom4')
             .setScale(0.8, 0.8)
 
         this.input.keyboard.on('keydown-SPACE', () => {
