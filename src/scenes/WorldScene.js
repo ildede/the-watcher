f81e5c085ed3799fd2ba8d8216240cfbc721953c
Davide Pedron <davide@happydev.cloud>
2020-05-05 04:55:09 +0200

    Disable debug graphic

--- src/scenes/WorldScene.js
+++ src/scenes/WorldScene.js
@@ -181,32 +181,32 @@ export default class WorldScene extends Phaser.Scene {
         this.cursors = this.input.keyboard.createCursorKeys();
         this.input.keyboard.on('keydown_SPACE', this.playerAction());
 
-        console.debug('D: Turn on physics debugging to show player\'s hitbox')
-        this.input.keyboard.once("keydown_D", event => {
-            // Turn on physics debugging to show player's hitbox
-            this.physics.world.createDebugGraphic();
-
-            // Create worldLayer collision graphic above the player, but below the help text
-            const graphics = this.add
-                .graphics()
-                .setAlpha(0.75)
-                .setDepth(20);
-            worldUp.renderDebug(graphics, {
-                tileColor: null, // Color of non-colliding tiles
-                collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-                faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-            });
-            worldMiddle.renderDebug(graphics, {
-                tileColor: null, // Color of non-colliding tiles
-                collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-                faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-            });
-            worldBottom.renderDebug(graphics, {
-                tileColor: null, // Color of non-colliding tiles
-                collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-                faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-            });
-        });
+        // console.debug('D: Turn on physics debugging to show player\'s hitbox')
+        // this.input.keyboard.once("keydown_D", event => {
+        //     // Turn on physics debugging to show player's hitbox
+        //     this.physics.world.createDebugGraphic();
+        //
+        //     // Create worldLayer collision graphic above the player, but below the help text
+        //     const graphics = this.add
+        //         .graphics()
+        //         .setAlpha(0.75)
+        //         .setDepth(20);
+        //     worldUp.renderDebug(graphics, {
+        //         tileColor: null, // Color of non-colliding tiles
+        //         collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+        //         faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+        //     });
+        //     worldMiddle.renderDebug(graphics, {
+        //         tileColor: null, // Color of non-colliding tiles
+        //         collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+        //         faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+        //     });
+        //     worldBottom.renderDebug(graphics, {
+        //         tileColor: null, // Color of non-colliding tiles
+        //         collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+        //         faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+        //     });
+        // });
 
         this.events.on('dialogStart', (box) => {
             this.dialogOpen = true
